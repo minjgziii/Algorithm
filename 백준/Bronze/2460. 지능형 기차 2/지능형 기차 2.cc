5867016eaa4b62ad412ca8d5186c7a4949e1b375
minjgziii <104371003+minjgziii@users.noreply.github.com>
2022-08-04 23:54:41 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int plus = 0;
+	int minus = 0;
+	vector<int> v;
+	int num = 0;
+
+	for (int i{ 0 }; i < 10; i++) {
+		cin >> minus >> plus;
+		num += -minus + plus;
+		v.push_back(num);
+	}
+	int max = *max_element(v.begin(), v.end());
+
+	cout << max;
+
+	return 0;
+}
