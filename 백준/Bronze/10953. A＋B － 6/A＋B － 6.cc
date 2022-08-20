56c62bbee75133995bf7961a17ca6c8902679148
minjgziii <104371003+minjgziii@users.noreply.github.com>
2022-08-20 23:57:40 +0900

    [Bronze II] Title: A+B - 6, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int T = 0;
+	cin >> T;
+	int a = 0;
+	char b = 0;
+	int c = 0;
+
+
+	for (int i{ 0 }; i < T; i++) {
+		cin >> a >> b >> c;
+
+		cout << a + c << endl;
+	}
+}
