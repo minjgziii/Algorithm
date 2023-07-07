b4ceaa082a3d439744ccaf3ded87ff2604150ebe
minjgziii <104371003+minjgziii@users.noreply.github.com>
2023-07-07 13:37:59 +0900

    [Gold IV] Title: 오큰수, Time: 220 ms, Memory: 9128 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<stack>
-#include<deque>
 using namespace std;
 
 int main() {
@@ -10,9 +9,10 @@ int main() {
 
 	int A = 0; // 수열 A의 크기
 	int number;
-	stack<int> s;
-	stack<int> answer;
-	stack<int> s1;
+	stack<int> s; // 오큰수를 구할 원소들
+	stack<int> s1; // 현재 원소의 오큰수 후보들
+	stack<int> answer; // 구한 원소들 (답)
+	
 
 	cin >> A;
 	
@@ -26,19 +26,19 @@ int main() {
 	answer.push(-1);
 
 	while (s.size() > 0) {
+		// s1에 원소가 하나도 없는 경우 -> 오큰수가 없음
 		if (s1.size() == 0) {
-			answer.push(-1);
-			s1.push(s.top());
+			answer.push(-1); // 오큰수 없기 때문에 답을 넣을 스택에 -1 push
+			s1.push(s.top()); // 다음 원소 비교를 위해 s1에 s의 top 원소 삽입
 			s.pop();
 		}
+		// s의 top보다 s1의 top이 큰 경우 -> 오큰수 후보/
 		else if (s.top() < s1.top()) {
 			answer.push(s1.top());
 			s1.push(s.top());
 			s.pop();
 		}
-		else if (s1.size() == 0) {
-
-		}
+		// 오큰수가 없지만, s1의 size가 0이 아닌 경우 (아직 오큰수가 될 수 있는 후보가 있음)
 		else {
 			s1.pop();
 		}
