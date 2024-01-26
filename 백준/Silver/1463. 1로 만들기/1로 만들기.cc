1541ff1674130f188d84d69b151a36f206381f64
minjgziii <104371003+minjgziii@users.noreply.github.com>
2024-01-26 18:40:37 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int N;
+int dp[1000000];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+
+	dp[0] = 0;
+	dp[1] = 0;
+
+	for (int i{ 2 }; i <= N; i++) {
+		dp[i] = dp[i - 1] + 1;
+		if (i % 2 == 0) {
+			dp[i] = min(dp[i], dp[i / 2] + 1);
+		}
+		if (i % 3 == 0) {
+			dp[i] = min(dp[i], dp[i / 3] + 1);
+		}
+	}
+
+	cout << dp[N];
+}
