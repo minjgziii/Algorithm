52f51775361e7a31bb8ae67bba07ed8039ba1048
minjgziii <104371003+minjgziii@users.noreply.github.com>
2024-02-21 23:24:56 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -2,5 +2,5 @@ SELECT I.ANIMAL_ID, I.ANIMAL_TYPE, I.NAME
 FROM ANIMAL_INS AS I
 JOIN ANIMAL_OUTS AS O
 ON I.ANIMAL_ID = O.ANIMAL_ID
-WHERE I.SEX_UPON_INTAKE LIKE "Intact%" AND O.SEX_UPON_OUTCOME IN ("Spayed Female", "Spayed Male", "Neutered Male", "Neutered Female")
+WHERE I.SEX_UPON_INTAKE LIKE 'Intact%' AND O.SEX_UPON_OUTCOME IN ('Spayed Female', 'Spayed Male', 'Neutered Male', 'Neutered Female')
 ORDER BY I.ANIMAL_ID
