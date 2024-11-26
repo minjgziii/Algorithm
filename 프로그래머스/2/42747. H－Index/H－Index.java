2c3f6dc1fade60826942ed0bef8773cfdfd7b961
minjgziii <104371003+minjgziii@users.noreply.github.com>
2024-11-26 16:41:22 +0900

    [level 2] Title: H-Index, Time: 13.22 ms, Memory: 92.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        
+        int size = citations.length;
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<=size; i++) {
+            int cnt = 0; // i번 이상 인용된 논문의 개수
+            int cnt_not = 0; // i번 이하 인용된 논문의 개수
+            
+            for(int j=0; j<size; j++) {
+                if(citations[j] >= i) {
+                    cnt++;
+                }
+                else {
+                    cnt_not++;
+                }
+            }
+            
+            if(cnt >= i) {
+                list.add(i);
+            }
+        }
+        
+        Collections.sort(list);
+        
+        answer = list.get(list.size()-1);
+        
+        return answer;
+    }
+}
